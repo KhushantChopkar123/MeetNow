2e5611678610463cc1862e7269d83641c751e574
khushantchopkar123 <khushantchopkar646@gmail.com>
2026-05-09 20:21:00 +0530

    Deployment frontend

--- frontend/vite-project/src/env.js
+++ frontend/vite-project/src/env.js
@@ -1,6 +1,6 @@
-let IS_PROD = false;
+
 const server = import.meta.env.MODE === "production"
-  ? "https://apnacollegebackend.onrender.com"
+  ? "https://meetnow-gjgc.onrender.com"
   : "http://localhost:8000";
 
 
